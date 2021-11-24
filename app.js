f6043eb8ddf7d1e5d73443d1c75fbd0160b47aec
joemoore1501 <90767241+joemoore1501@users.noreply.github.com>
2021-11-24 12:55:21 +0000

    Server update and website page created Server now updates whenever changes are made and public folder with html and css inside created

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 //creating app
 const app = express();
-//send an HTTP response when receiving HTTP GET /
+//send the index.html when receiving HTTP GET /
 app.get("/", (req, res) => {
-  res.send("Hello, your server is up and running");
+  res.sendFile("public/index.html", { root: __dirname });
 });
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
