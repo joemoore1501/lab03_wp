deed06071afdf1900578aeb69e81badb397d7286
joemoore1501 <90767241+joemoore1501@users.noreply.github.com>
2021-11-24 13:19:43 +0000

    index.html split into 3 .ejs files

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 //creating app
 const app = express();
-//send the index.html when receiving HTTP GET /
+//handling static HTML and EJS templates
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.sendFile("public/index.html", { root: __dirname });
+  res.render("index"); //no need for ejs extension
 });
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
