a46c7267c45ec3a4291218a674316c382e422160
joemoore1501 <90767241+joemoore1501@users.noreply.github.com>
2021-11-25 12:47:10 +0000

    Created login, register and contacts page Also removed index.html file

--- app.js
+++ app.js
@@ -4,9 +4,26 @@ const app = express();
 //handling static HTML and EJS templates
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.render("index"); //no need for ejs extension
 });
+
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
+});
+
+//route for register
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+//route for login
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
